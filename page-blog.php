a7e5174ff5949ca6cca832d8797a9b349df273ec
tiMorpheus <tymurtolochko@gmail.com>
2019-02-06 18:34:48 +0200

    layout css

--- page-blog.php
+++ page-blog.php
@@ -63,7 +63,7 @@
                         <h5 class="post-blog-title" ><?php the_title(); ?></h5>
                     </a>
 
-                    <p class="blog-post-data">
+                    <p class="blog-post-date">
                         <?php the_time('F j, Y'); ?>
                     </p>
 
