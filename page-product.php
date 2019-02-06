e7a5a1655cc127a7806afda3ef33ea98d8c80fb7
tiMorpheus <tymurtolochko@gmail.com>
2019-02-06 15:28:18 +0200

    products page

--- page-product.php
+++ page-product.php
@@ -44,7 +44,7 @@
                 </div>
 
                 <h2 class="product-title">
-
+                    Shared Proxies
                 </h2>
 
                 <div class="product-description">
@@ -52,6 +52,76 @@
                         <b>Shared proxies are split between 3 users.</b>
                         You could be potentially sharing with up to two other users on a proxy. This may cause some sites to be blocked if another user has used the proxy on said site. This allows us to provide proxies at a significantly lower price. Not for use for social media accounts.
                     </p>
+                </div>
+
+                <a href="" class="product-link">
+
+                    Order now
+                </a>
+            </div>
+            <div class="item-50">
+
+                <div class="product-img">
+
+                </div>
+
+                <h2 class="product-title">
+                    Private Proxies
+                </h2>
+
+                <div class="product-description">
+                    <p>
+                        <b> Private proxies are only used by a single user (you).</b>
+                        The proxy will not be shared with any other users. These proxies are better suited for social media accounts and other dedicated purposes.
+                    </p>
+
+                </div>
+
+                <a href="" class="product-link">
+
+                    Order now
+                </a>
+            </div>
+            <div class="item-50">
+
+                <div class="product-img">
+
+                </div>
+
+                <h2 class="product-title">
+                    Rotating Proxies
+                </h2>
+
+                <div class="product-description">
+                    <p>
+                        <b>Rotating proxies will provide you with ports that will change per IP given.</b>
+                        You may select for each proxy to rotate from 10-120 minutes. When you connect to the port, the proxy will change automatically for you to the time you set.
+                    </p>
+
+                </div>
+
+                <a href="" class="product-link">
+
+                    Order now
+                </a>
+            </div>
+            <div class="item-50">
+
+                <div class="product-img">
+
+                </div>
+
+                <h2 class="product-title">
+                    Shoe Proxies
+                </h2>
+
+                <div class="product-description">
+                    <p>
+                        <b>Shoe proxies are optimized for accessing footsites since they are in a hosted in datacenters near major footsites.</b>
+                        These proxies are only used by you so you will be connected instantly.
+                    </p>
+
+
                 </div>
 
                 <a href="" class="product-link">
