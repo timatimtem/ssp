eb229488b6cda8fa122d42f237e6da18653ae6e9
tiMorpheus <tymurtolochko@gmail.com>
2019-02-06 13:23:17 +0200

    home section styled

--- page-about-us.php
+++ page-about-us.php
@@ -5,12 +5,37 @@
 
 
 
+<section class="about-us-section">
 
+
+
+    <div class="container">
+
+
+        <h1 class="section-title">
+            About us
+        </h1>
+
+        <h3 class="section-subtitle">
+            The best place to buy proxies
+        </h3>
+
+
+    </div>
+
+
+
+
+</section>
+
+
+
+
+
+<?php get_footer();?>
 
 
 <?php the_post() ?>
 <?php the_content(); ?>
 
 
-
-<?php get_footer();?>
