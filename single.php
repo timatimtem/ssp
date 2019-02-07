302f00f58d254fdee3a82a4d20098f075aaed8f2
tiMorpheus <tymurtolochko@gmail.com>
2019-02-07 16:54:21 +0200

    hovers

--- single.php
+++ single.php
@@ -76,7 +76,18 @@
                 </div>
 
                 <div class="recent-posts-wrap">
-                    <?php if ( function_exists( "get_yuzo_related_posts" ) ) { get_yuzo_related_posts(); } ?>
+
+                    <h2>Recent Posts</h2>
+                    <ul>
+                        <?php
+                        $args = array( 'numberposts' => '2' );
+                        $recent_posts = wp_get_recent_posts( $args );
+                        foreach( $recent_posts as $recent ){
+                            echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+                        }
+                        wp_reset_query();
+                        ?>
+                    </ul>
                 </div>
             </div>
 
